325e8d87b8c7afb50ccf64f6adfd6d048a69be4a
GerodotCode <abcitycg@gmail.com>
2024-04-10 12:45:02 +0300

    Optimize e2e/clients/vue/App.vue

---
+++ e2e/clients/vue/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <dashboard :BigDeal="BigDeal" />
+</template>
+
+<script>
+import Vue from 'vue'
+import BigDeal from '@BigDeal/core'
+import { Dashboard } from '@BigDeal/vue'
+
+export default {
+  name: 'App',
+  components: {
+    Dashboard,
+  },
+  computed: {
+    BigDeal: () => new BigDeal(),
+  },
+}
+</script>
+
+<style src="@BigDeal/core/dist/style.css"></style>
+<style src="@BigDeal/dashboard/dist/style.css"></style>
