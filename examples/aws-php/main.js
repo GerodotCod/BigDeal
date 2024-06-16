c28fe920b9c9c7194b3c90d4267ace77605537f8
GerodotCode <abcitycg@gmail.com>
2024-06-16 16:11:01 +0300

    Experimenting examples/aws-php/main.js

---
+++ examples/aws-php/main.js
@@ -0,0 +1,42 @@
+import BigDeal from '@BigDeal/core'
+import Dashboard from '@BigDeal/dashboard'
+import AwsS3 from '@BigDeal/aws-s3'
+
+const BigDeal = new BigDeal({
+  debug: true,
+})
+
+BigDeal.use(Dashboard, {
+  inline: true,
+  target: 'body',
+})
+BigDeal.use(AwsS3, {
+  shouldUseMultipart: false, // The PHP backend only supports non-multipart uploads
+
+  getUploadParameters (file) {
+    // Send a request to our PHP signing endpoint.
+    return fetch('/s3-sign.php', {
+      method: 'post',
+      // Send and receive JSON.
+      headers: {
+        accept: 'application/json',
+        'content-type': 'application/json',
+      },
+      body: JSON.stringify({
+        filename: file.name,
+        contentType: file.type,
+      }),
+    }).then((response) => {
+      // Parse the JSON response.
+      return response.json()
+    }).then((data) => {
+      // Return an object in the correct shape.
+      return {
+        method: data.method,
+        url: data.url,
+        fields: data.fields,
+        headers: data.headers,
+      }
+    })
+  },
+})
